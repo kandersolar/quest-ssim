e3730f32b84bfd80d06e1e9ed01f8682d7de4f36
Kevin Anderson <kevin.anderso@gmail.com>
2024-10-30 22:47:34 -0400

    more testing

--- reproduce_segfault.py
+++ reproduce_segfault.py
@@ -2,11 +2,6 @@
 import faulthandler
 faulthandler.enable()  # just to show a bit of info about the segfault
 
-from helics import (
-    HelicsFederate,
-#    HelicsValueFederate,
-#    helics_time_maxtime,
-#    helicsCreateValueFederateFromConfig
-)
+import helics
 
 import matplotlib.pyplot as plt
