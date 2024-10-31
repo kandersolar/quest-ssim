70d920c3d651921fbc3019d00a90d882721f314b
Kevin Anderson <kevin.anderso@gmail.com>
2024-10-30 22:29:34 -0400

    instrument the reproducer

--- reproduce_segfault.py
+++ reproduce_segfault.py
@@ -1,13 +1,21 @@
 
 
+print("abc", flush=True)
 from abc import ABC, abstractmethod
+print("argparse", flush=True)
 import argparse
+print("csv", flush=True)
 import csv
+print("json", flush=True)
 import json
+print("logging", flush=True)
 import logging
+print("pathlib", flush=True)
 from pathlib import Path
+print("typing", flush=True)
 from typing import Set, List
 
+print("helics", flush=True)
 from helics import (
     HelicsFederate,
     HelicsValueFederate,
@@ -15,6 +23,8 @@ from helics import (
     helicsCreateValueFederateFromConfig
 )
 
+print("matplotlib", flush=True)
 import matplotlib.pyplot as plt
 
+print("ssim.federates.timing", flush=True)
 from ssim.federates import timing
