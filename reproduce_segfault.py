1b62369e3734f4f4375eb5efdbf77a37a0ae1ceb
Kevin Anderson <kevin.anderso@gmail.com>
2024-10-30 22:32:54 -0400

    further isolate and instrument

--- reproduce_segfault.py
+++ reproduce_segfault.py
@@ -1,30 +1,5 @@
 
+import faulthandler
+faulthandler.enable()
 
-print("abc", flush=True)
-from abc import ABC, abstractmethod
-print("argparse", flush=True)
-import argparse
-print("csv", flush=True)
-import csv
-print("json", flush=True)
-import json
-print("logging", flush=True)
-import logging
-print("pathlib", flush=True)
-from pathlib import Path
-print("typing", flush=True)
-from typing import Set, List
-
-print("helics", flush=True)
-from helics import (
-    HelicsFederate,
-    HelicsValueFederate,
-    helics_time_maxtime,
-    helicsCreateValueFederateFromConfig
-)
-
-print("matplotlib", flush=True)
 import matplotlib.pyplot as plt
-
-print("ssim.federates.timing", flush=True)
-from ssim.federates import timing
