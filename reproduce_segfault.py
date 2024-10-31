6a866a387d92f0732d2a72521afb55c126706865
Kevin Anderson <kevin.anderso@gmail.com>
2024-10-30 22:39:20 -0400

    partially resimplify

--- reproduce_segfault.py
+++ reproduce_segfault.py
@@ -2,20 +2,6 @@
 import faulthandler
 faulthandler.enable()
 
-print("abc", flush=True)
-from abc import ABC, abstractmethod
-print("argparse", flush=True)
-import argparse
-print("csv", flush=True)
-import csv
-print("json", flush=True)
-import json
-print("logging", flush=True)
-import logging
-print("pathlib", flush=True)
-from pathlib import Path
-print("typing", flush=True)
-from typing import Set, List
 
 print("helics", flush=True)
 from helics import (
